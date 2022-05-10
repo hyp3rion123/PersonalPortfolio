d0950bebf3a07bf5d6611af426febbda3296f5e3
hyp3rion123 <andreigli@yahoo.com>
2022-05-09 21:52:28 -0400

    Initial commit

---
+++ build/precache-manifest.c98b82ab572c1d8561cc736ade35574c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "474d6b745387781325424fe765d6e77b",
+    "url": "/PersonalPortfolio/index.html"
+  },
+  {
+    "revision": "3795154655cdc4b7b308",
+    "url": "/PersonalPortfolio/static/css/2.45c371c4.chunk.css"
+  },
+  {
+    "revision": "7842031758c65c8b28da",
+    "url": "/PersonalPortfolio/static/css/main.e8901382.chunk.css"
+  },
+  {
+    "revision": "3795154655cdc4b7b308",
+    "url": "/PersonalPortfolio/static/js/2.2baeab8a.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/PersonalPortfolio/static/js/2.2baeab8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7842031758c65c8b28da",
+    "url": "/PersonalPortfolio/static/js/main.31211222.chunk.js"
+  },
+  {
+    "revision": "96ef04ec83fac0145622",
+    "url": "/PersonalPortfolio/static/js/runtime-main.6257c7f6.js"
+  }
+]);
